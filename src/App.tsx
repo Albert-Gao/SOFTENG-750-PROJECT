a03d2dddf43a2b9f5c9827eb85d2922cd72485b0
Albert Gao <albertgaohy@gmail.com>
2021-03-16 20:26:41 +1300

    feat: added react routes and route level code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavBar } from './components/NavBar'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes } from './routes/routes'
 
 const App: React.FC = () => {
     return (
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+            <NavBar />
+            <Routes />
+        </BrowserRouter>
     )
 }
 
