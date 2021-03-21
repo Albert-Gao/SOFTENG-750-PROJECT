787e7d48efe0f3b33bac09223a4554cac0c80c47
Albert Gao <albertgaohy@gmail.com>
2021-03-21 19:00:13 +1300

    fix: pm2 config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,23 +4,18 @@ module.exports = {
             script: './lib/index.js',
             watch: '.',
         },
-        {
-            script: './service-worker/',
-            watch: ['./service-worker'],
-        },
     ],
-
-    deploy: {
-        production: {
-            user: 'SSH_USERNAME',
-            host: 'SSH_HOSTMACHINE',
-            ref: 'origin/master',
-            repo: 'GIT_REPOSITORY',
-            path: 'DESTINATION_PATH',
-            'pre-deploy-local': '',
-            'post-deploy':
-                'npm install && pm2 reload ecosystem.config.js --env production',
-            'pre-setup': '',
-        },
-    },
+    // deploy: {
+    //     production: {
+    //         user: 'SSH_USERNAME',
+    //         host: 'SSH_HOSTMACHINE',
+    //         ref: 'origin/master',
+    //         repo: 'GIT_REPOSITORY',
+    //         path: 'DESTINATION_PATH',
+    //         'pre-deploy-local': '',
+    //         'post-deploy':
+    //             'npm install && pm2 reload ecosystem.config.js --env production',
+    //         'pre-setup': '',
+    //     },
+    // },
 }
