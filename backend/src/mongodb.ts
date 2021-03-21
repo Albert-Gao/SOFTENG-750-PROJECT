8d7fcff0edd867083485749586b3282275175c3b
Albert Gao <albertgaohy@gmail.com>
2021-03-21 18:04:23 +1300

    fix: MongoDB Atlas config

--- backend/src/mongodb.ts
+++ backend/src/mongodb.ts
@@ -6,6 +6,7 @@ export default function (app: Application): void {
     const database = connection.substr(connection.lastIndexOf('/') + 1)
     const mongoClient = MongoClient.connect(connection, {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
     }).then((client) => client.db(database))
 
     app.set('mongoClient', mongoClient)
