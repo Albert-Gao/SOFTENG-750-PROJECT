3348df37943e08147b5f392ebcd48872bd463e42
AlbertGao <albertgaohy@gmail.com>
2021-03-21 16:45:24 +1300

    feat: started using Heroku (#18)

--- backend/src/services/news/news.class.ts
+++ backend/src/services/news/news.class.ts
@@ -1,19 +1,18 @@
-import { Params } from '@feathersjs/feathers'
+import { Params, Id } from '@feathersjs/feathers'
 import { Service, MongooseServiceOptions } from 'feathers-mongoose'
 import { Application } from '../../declarations'
 import { NewsDocument } from '../../models/news.model'
+
+// async find(params: Params) {}
+// async get(id: Id, params: Params) {}
+// async create(data: any, params: Params) {}
+// async update(id: NullableId, data: any, params: Params) {}
+// async patch(id: NullableId, data: any, params: Params) {}
+// async remove(id: NullableId, params: Params) {}
 
 export class News extends Service<NewsDocument> {
     //eslint-disable-next-line @typescript-eslint/no-unused-vars
     constructor(options: Partial<MongooseServiceOptions>, app: Application) {
         super(options)
-    }
-
-    async create(data: any, params: Params) {
-        const news = await this._create({
-            title: data.title,
-        })
-
-        return Promise.resolve(news)
     }
 }
