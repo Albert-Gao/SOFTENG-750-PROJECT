15a953c8170943da7a4506a25e9542cf9540f207
Albert Gao <albertgaohy@gmail.com>
2021-04-04 23:46:16 +1200

    feat: added submit news backend
    close #30

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,6 +32,7 @@ app.use(
         contentSecurityPolicy: false,
     }),
 )
+
 app.use(cors())
 app.use(compress())
 app.use(express.json())
--- backend/test/services/news.test.ts
+++ backend/test/services/news.test.ts
@@ -1,8 +1,19 @@
-import app from '../../src/app';
+import app from '../../src/app'
+import { describe, expect, it } from '@jest/globals'
 
-describe('\'news\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('news');
-    expect(service).toBeTruthy();
-  });
-});
+describe('"news" service', () => {
+    it('registered the service', () => {
+        const service = app.service('news')
+        expect(service).toBeTruthy()
+    })
+
+    it('should throw 400 when the request sender is not matching the request body object id', async () => {
+        const service = app.service('news')
+
+        async function call() {
+            return service.create({ wikipediaUrl: '', author: '' })
+        }
+
+        expect(call()).toThrow()
+    })
+})
