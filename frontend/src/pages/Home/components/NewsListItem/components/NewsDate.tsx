c9923dc2d4846301c19f69bebf94f8f5965390df
AlbertGao <albertgaohy@gmail.com>
2021-04-08 16:19:27 +1200

    feat: added the relative date time display and util functions (#45)

--- frontend/src/pages/Home/components/NewsListItem/components/NewsDate.tsx
+++ frontend/src/pages/Home/components/NewsListItem/components/NewsDate.tsx
@@ -1,7 +1,15 @@
+import { DateTime } from '../../../../../utils/DateTime'
+
 export const NewsDate: React.FC<{ dateString: string }> = ({ dateString }) => (
     <p className="text-sm text-gray-500">
-        <a href="#" className="hover:underline">
-            <time dateTime="2020-12-09T11:43:00">{dateString}</time>
+        <a
+            href="#"
+            className="hover:underline"
+            title={DateTime.formatToLocal(dateString)}
+        >
+            <time dateTime="2020-12-09T11:43:00">
+                {DateTime.utc2LocalRelative(dateString)}
+            </time>
         </a>
     </p>
 )
