73ad970d06517c2f3eedaa2219dbe72bd9ac0280
AlbertGao <albertgaohy@gmail.com>
2021-04-08 20:47:33 +1200

    feat: added features page, content needs to be rewritten (#50)

--- frontend/src/pages/Features/Features.tsx
+++ frontend/src/pages/Features/Features.tsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import { FeatureHeader } from './components/FeatureHeader'
+import { FeatureSlot } from './components/FeatureSlot'
 
-const Features: React.FC = () => {
-    return <div>Features</div>
-}
+const Features: React.FC = () => (
+    <div className="relative py-16 bg-white sm:py-24 lg:py-32">
+        <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+            <FeatureHeader />
+            <div className="mt-12">
+                <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                    <FeatureSlot
+                        iconType="deploy"
+                        title="Push to Deploy"
+                        description="Ac tincidunt sapien vehicula erat auctor
+                            pellentesque rhoncus. Et magna sit morbi
+                            lobortis."
+                    />
+
+                    <FeatureSlot
+                        iconType="lock"
+                        title="SSL Certificates"
+                        description="Ac tincidunt sapien vehicula erat auctor
+                            pellentesque rhoncus. Et magna sit morbi
+                            lobortis."
+                    />
+
+                    <FeatureSlot
+                        iconType="refresh"
+                        title="Simple Queues"
+                        description="Ac tincidunt sapien vehicula erat auctor
+                            pellentesque rhoncus. Et magna sit morbi
+                            lobortis."
+                    />
+
+                    <FeatureSlot
+                        iconType="shield"
+                        title="Advanced Security"
+                        description="Ac tincidunt sapien vehicula erat auctor
+                            pellentesque rhoncus. Et magna sit morbi
+                            lobortis."
+                    />
+
+                    <FeatureSlot
+                        iconType="cogwheel"
+                        title=" Powerful API"
+                        description="Ac tincidunt sapien vehicula erat auctor
+                            pellentesque rhoncus. Et magna sit morbi
+                            lobortis."
+                    />
+
+                    <FeatureSlot
+                        iconType="shelf"
+                        title="Database Backups"
+                        description="Ac tincidunt sapien vehicula erat auctor
+                            pellentesque rhoncus. Et magna sit morbi
+                            lobortis."
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+)
 
 export default Features
---
+++ frontend/src/pages/Features/components/FeatureHeader.tsx
@@ -0,0 +1,15 @@
+export const FeatureHeader: React.FC = () => (
+    <>
+        <h2 className="text-base font-semibold tracking-wider text-indigo-600 uppercase">
+            Deploy faster
+        </h2>
+        <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Everything you need to deploy your app
+        </p>
+        <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
+            Phasellus lorem quam molestie id quisque diam aenean nulla in.
+            Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
+            condimentum id viverra nulla.
+        </p>
+    </>
+)
