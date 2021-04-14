775e6273272b5af91ffed1bf64d885b0a808fcd4
AlbertGao <albertgaohy@gmail.com>
2021-04-15 11:16:10 +1200

    feat: now the profile/profile route works (#107)

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,20 +1,46 @@
 import { useAtom } from 'jotai'
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useMutation } from 'react-query'
+import { useToasts } from 'react-toast-notifications'
+import { changeUserInfoApi } from '../../api/users.api'
+import { FormErrorText } from '../../components/FormErrorText'
 import { ProfileSectionSubmitButton } from '../../components/ProfileSectionSubmitButton'
 import { userAtom } from '../../state'
 import { getAvatarSVG } from '../../utils/getAvatarSVG'
 import { ChangeAvatarModal } from './components/ChangeAvatarModal'
 
 const Profile: React.FC = () => {
-    const [{ email, avatar }] = useAtom(userAtom)
+    const { addToast } = useToasts()
+    const [{ email, avatar, nickName }, setUserAtom] = useAtom(userAtom)
     const [isOpen, setIsOpen] = useState(false)
-
+    const mutation = useMutation(changeUserInfoApi.query)
+    const formInfo = useForm({
+        defaultValues: {
+            nickName,
+        },
+    })
+    console.log(formInfo.errors)
     return (
         <>
             <form
                 className="divide-y divide-gray-200 lg:col-span-9"
-                action="#"
-                method="POST"
+                onSubmit={formInfo.handleSubmit(async (values) => {
+                    try {
+                        await mutation.mutateAsync({
+                            nickName: values.nickName,
+                        })
+                        setUserAtom((prev) => ({
+                            ...prev,
+                            nickName: values.nickName,
+                        }))
+                    } catch (e) {
+                        addToast(
+                            'Something wrong happened when submitting, please try again later',
+                            { appearance: 'success' },
+                        )
+                    }
+                })}
             >
                 {/* <!-- Profile section --> */}
                 <div className="px-4 py-6 sm:p-6 lg:pb-8">
@@ -56,12 +82,23 @@ const Profile: React.FC = () => {
                                     Nick name
                                 </label>
                                 <input
+                                    ref={formInfo.register({
+                                        required: 'nick name can not be empty',
+                                        max: {
+                                            value: 20,
+                                            message:
+                                                'maxmum length of nick name is 20',
+                                        },
+                                    })}
                                     type="text"
-                                    name="first_name"
-                                    id="first_name"
-                                    autoComplete="given-name"
+                                    name="nickName"
                                     className="block w-1/2 px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-light-blue-500 focus:border-light-blue-500 sm:text-sm"
                                 />
+                                <FormErrorText
+                                    isError={!!formInfo.errors.nickName}
+                                >
+                                    {formInfo.errors.nickName?.message}
+                                </FormErrorText>
                             </div>
                         </div>
 
@@ -116,7 +153,9 @@ const Profile: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <ProfileSectionSubmitButton />
+                <ProfileSectionSubmitButton
+                    disabled={mutation.isLoading || !formInfo.formState.isDirty}
+                />
             </form>
         </>
     )
