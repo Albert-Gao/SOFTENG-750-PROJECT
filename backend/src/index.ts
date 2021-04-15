ef63e89596ad7cbcb8eb2fcfe84a92b888a20dc2
Albert Gao <albertgaohy@gmail.com>
2021-04-15 16:46:46 +1200

    fix: Heroku env vars

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,13 +7,12 @@ const server = app.listen(port)
 process.on('unhandledRejection', (reason, p) =>
     logger.error('Unhandled Rejection at: Promise ', p, reason),
 )
-
+console.log('process.env.PORT', process.env.PORT)
 server.on('listening', () =>
     logger.info(
         'Feathers application started on http://%s:%d',
         app.get('host'),
-        process.env.NODE_ENV === 'production'
-            ? process.env.PORT
-            : app.get('port'),
+
+        process.env.PORT != null ? process.env.PORT : app.get('port'),
     ),
 )
