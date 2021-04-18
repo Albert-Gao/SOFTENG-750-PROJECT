0b0a34f5086b4277f6038cd1e3dae2c93d763928
Albert Gao <albertgaohy@gmail.com>
2021-04-18 13:13:36 +1200

    chore: console log all error message

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
             script: './lib/index.js',
             watch: '.',
             env: {
+                DEBUG: '*',
                 NODE_ENV: 'production',
             },
         },
