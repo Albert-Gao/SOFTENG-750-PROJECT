7a022c7f69ea48aeea0b43de52310fcd8affa3be
EVAN <yzho976@aucklanduni.ac.nz>
2021-05-01 18:38:06 +1200

    Implement login storybook view

--- frontend/src/stories/Login.stories.tsx
+++ frontend/src/stories/Login.stories.tsx
@@ -2,23 +2,14 @@ import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
 
-import { Login, LoginProps } from './Login'
+import { LoginPage, LoginProps } from './LoginPage'
 
 export default {
     title: 'Example/Login',
-    component: Login,
+    component: LoginPage,
 } as Meta
 
-const Template: Story<LoginProps> = (args) => <Login {...args} />
+const Template: Story<LoginProps> = (args) => <LoginPage {...args} />
 
-export const LogInButton = Template.bind({})
-LogInButton.args = {
-    loginButton: true,
-    textBox: false,
-}
-
-export const TextBox = Template.bind({})
-TextBox.args = {
-    loginButton: false,
-    textBox: true,
-}
+export const LogInPage = Template.bind({})
+LogInPage.args = {}
