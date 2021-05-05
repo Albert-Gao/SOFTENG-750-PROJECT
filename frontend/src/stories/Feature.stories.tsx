6961d6dadb65ecd9bcef39004a540ab0855b26c0
EVAN <yzho976@aucklanduni.ac.nz>
2021-05-05 19:38:26 +1200

    Adapt newly introduced util file

--- frontend/src/stories/Feature.stories.tsx
+++ frontend/src/stories/Feature.stories.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
-
-import { Features, FeatureProps } from './FeaturePage'
+import Features from '../pages/Features/Features'
+import { getStoryTemplate, Meta } from './utils'
 
 export default {
     title: 'Example/Features',
     component: Features,
 } as Meta
 
-const Template: Story<FeatureProps> = (args) => <Features {...args} />
+const Template = getStoryTemplate(Features)
 
 export const FeaturePage = Template.bind({})
 FeaturePage.args = {}
