d1c420809d70ec149fff11df7619121225ecbe37
syan386 <55684460+syan386@users.noreply.github.com>
2021-05-05 17:21:52 +0800

    Register storybook change
    Register storybook change

--- frontend/src/stories/Register.stories.tsx
+++ frontend/src/stories/Register.stories.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
-
-import { Register2, RegisterProps } from './RegisterPage'
+import Register from '../pages/Register/Register'
+import { getStoryTemplate, Meta } from './utils'
 
 export default {
-    title: 'Example/Register',
-    component: Register2,
+    title: 'Pages/Register',
+    component: Register,
 } as Meta
 
-const Template: Story<RegisterProps> = (args) => <Register2 {...args} />
+const Template = getStoryTemplate(Register)
 
-export const RegisterPage = Template.bind({})
-RegisterPage.args = {}
+export const Standard = Template.bind({})
+Standard.args = {}
