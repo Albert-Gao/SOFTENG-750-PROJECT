1dbc85e53fbf934479a902aa6cdc31db5a320029
EVAN <yzho976@aucklanduni.ac.nz>
2021-05-08 19:38:16 +1200

    Add profile page into storyboard

--- frontend/src/stories/Profilesetting.stories.tsx
+++ frontend/src/stories/Profilesetting.stories.tsx
@@ -1,12 +1,26 @@
-import User from '../pages/User/User'
+import Profile from '../pages/Profile/Profile'
+import { ProfilePageSideNav } from '../components/AppShell/components/ProfilePageSideNav'
+import { SettingsPageContainer } from '../components/AppShell/components/Main'
+import { PageHeader } from '../components/AppShell/components/PageHeader'
 import { getStoryTemplate, Meta } from './utils'
 
+const trial: React.FC = ({ children }) => {
+    return (
+        <div>
+            <PageHeader />
+            <SettingsPageContainer>
+                <ProfilePageSideNav />
+                <Profile />
+            </SettingsPageContainer>
+        </div>
+    )
+}
 export default {
     title: 'Pages/Profile',
-    component: User,
+    component: trial,
 } as Meta
 
-const Template = getStoryTemplate(User)
+const Template = getStoryTemplate(trial)
 
 export const ProfilePage = Template.bind({})
 ProfilePage.args = {}
