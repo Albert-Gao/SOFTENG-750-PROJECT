2b673558c60355491c61777ff25d73fb498005b8
Albert Gao <albertgaohy@gmail.com>
2021-05-08 13:41:57 +1200

    feat: added component tests to the <Login/>

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+window.alert = jest.fn()
+
+jest.mock('./api/auth.api')
+jest.mock('./api/comments.api')
+jest.mock('./api/news.api')
+jest.mock('./api/users.api')
