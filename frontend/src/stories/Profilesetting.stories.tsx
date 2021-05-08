83852885e4b11cdf19d59184d1c52a626a505e43
EVAN <yzho976@aucklanduni.ac.nz>
2021-05-08 20:13:43 +1200

    Add privacy page into storyboard

--- frontend/src/stories/Profilesetting.stories.tsx
+++ frontend/src/stories/Profilesetting.stories.tsx
@@ -1,4 +1,5 @@
 import Profile from '../pages/Profile/Profile'
+import Privacy from '../pages/ProfilePrivacy/ProfilePrivacy'
 import { ProfilePageSideNav } from '../components/AppShell/components/ProfilePageSideNav'
 import { SettingsPageContainer } from '../components/AppShell/components/Main'
 import { PageHeader } from '../components/AppShell/components/PageHeader'
@@ -15,12 +16,28 @@ const trial: React.FC = ({ children }) => {
         </div>
     )
 }
+const trial1: React.FC = ({ children }) => {
+    return (
+        <div>
+            <PageHeader />
+            <SettingsPageContainer>
+                <ProfilePageSideNav />
+                <Privacy />
+            </SettingsPageContainer>
+        </div>
+    )
+}
+
 export default {
     title: 'Pages/Profile',
     component: trial,
 } as Meta
 
 const Template = getStoryTemplate(trial)
+const Template1 = getStoryTemplate(trial1)
 
 export const ProfilePage = Template.bind({})
 ProfilePage.args = {}
+
+export const PrivacyPage = Template1.bind({})
+PrivacyPage.args = {}
