cc40c7862442408ccc22b000170f321f2e234d62
EVAN <yzho976@aucklanduni.ac.nz>
2021-05-08 23:52:48 +1200

    Add Home page into storybook

--- frontend/src/components/AppShell/components/Main.tsx
+++ frontend/src/components/AppShell/components/Main.tsx
@@ -3,7 +3,10 @@ import { useLocation } from 'react-router-dom'
 import { PATHS } from '../../../routes/routes.constants'
 import { ProfilePageSideNav } from './ProfilePageSideNav'
 
-const Container: React.FC<{ isAuth: boolean }> = ({ isAuth, children }) => (
+export const Container: React.FC<{ isAuth: boolean }> = ({
+    isAuth,
+    children,
+}) => (
     <main className={isAuth ? '-mt-32' : ''}>
         <div className="px-4 pb-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
             {children}
--- frontend/src/stories/Home.stories.tsx
+++ frontend/src/stories/Home.stories.tsx
@@ -1,15 +1,85 @@
-import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { Container } from '../components/AppShell/components/Main'
+import { SubmitButton } from '../components/AppShell/components/PageHeader'
+import { getStoryTemplate, Meta } from './utils'
+import { NewsListItem } from '../pages/Home/components/NewsListItem/NewsListItem'
 
-import { Homee, HomeProps } from './HomePage'
+const data = [
+    {
+        author: {
+            avatar: 'A50king',
+            createdAt: '2021-05-01T08:09:10.807Z',
+            email: '123456789@qq.com',
+            favourites: [],
+            nickName: '123456789',
+            privacy: {
+                shouldShowEmail: true,
+                shouldShowFavouritePage: true,
+                shouldShowSubmittedNews: true,
+            },
+            updatedAt: '2021-05-01T08:09:10.807Z',
+            _id: '608d0ca636814a003e2da1e9',
+        },
+        authorWords: 'xxx',
+        commentsCount: 0,
+        createdAt: '2021-05-08T08:21:22.264Z',
+        description:
+            " Sfoglia l'indice   Consulta il sommario    Naviga tra i portali tematici↵",
+        title: "Wikipedia, l'enciclopedia libera",
+        updatedAt: '2021-05-08T08:21:22.264Z',
+        votingRecords: [],
+        wikipediaUrl: 'https://it.wikipedia.org/wiki/Pagina_principale',
+        __v: 0,
+        _id: '60964a02df818d003d9b0f9a',
+    },
+]
+
+const trial: React.FC = ({ children }) => {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <div className={`pb-32 bg-indigo-600`}>
+                <header className="py-10">
+                    <div className="flex justify-between px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold text-white">Home</h1>
+
+                        <SubmitButton />
+                    </div>
+                </header>
+            </div>
+            <Container isAuth={true}>
+                <div className="px-1 py-6 rounded-lg sm:px-6">
+                    <ul className="flex flex-col items-center space-y-4">
+                        <li
+                            key="60964a02df818d003d9b0f9a"
+                            className="w-full max-w-2xl px-4 py-6 bg-white rounded-lg shadow sm:p-6"
+                        >
+                            <NewsListItem
+                                authorId="608d0ca636814a003e2da1e9"
+                                id="60964a02df818d003d9b0f9a"
+                                wikipediaUrl="https://it.wikipedia.org/wiki/Pagina_principale"
+                                authorName="123456789"
+                                newsDate="2021-05-08T08:21:22.264Z"
+                                title="Wikipedia, l'enciclopedia libera"
+                                authorWords="xxx"
+                                description=" Sfoglia l'indice   Consulta il sommario    Naviga tra i portali tematici↵"
+                                authorAvatar="A50king"
+                                commentsCount={0}
+                                votingRecords={[]}
+                                refetch={null}
+                            />
+                        </li>
+                    </ul>
+                </div>
+            </Container>
+        </div>
+    )
+}
 
 export default {
-    title: 'Example/Home',
-    component: Homee,
+    title: 'Pages/Home',
+    component: trial,
 } as Meta
 
-const Template: Story<HomeProps> = (args) => <Homee {...args} />
+const Template = getStoryTemplate(trial)
 
 export const HomePage = Template.bind({})
 HomePage.args = {}
