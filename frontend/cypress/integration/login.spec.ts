95039b5a9e5b8fdf5e709bf75c0bffd395248801
AlbertGao <albertgaohy@gmail.com>
2021-05-09 15:23:26 +1200

    feat: added e2e test for home page interactions (#198)
    close #197

--- frontend/cypress/integration/login.spec.ts
+++ frontend/cypress/integration/login.spec.ts
@@ -1,19 +1,23 @@
 describe('Login', () => {
     it('should NOT log the user in when things are right', () => {
-        cy.visit('https://softeng-750-project.vercel.app/login')
+        cy.visit('/login')
         cy.get('input[name=email]').type(`goodboy@goodboy.com`)
         cy.get('input[name=password]').type(`567`)
         cy.get('button[type=submit]').click()
 
-        cy.url().should('equal', 'https://softeng-750-project.vercel.app/login')
+        cy.location().should((location) => {
+            expect(location.pathname).equal('/login')
+        })
     })
 
     it('should log the user in when things are right', () => {
-        cy.visit('https://softeng-750-project.vercel.app/login')
+        cy.visit('/login')
         cy.get('input[name=email]').type(`goodboy@goodboy.com`)
         cy.get('input[name=password]').type(`123`)
         cy.get('button[type=submit]').click()
 
-        cy.url().should('eq', 'https://softeng-750-project.vercel.app/')
+        cy.location().should((location) => {
+            expect(location.pathname).equal('/')
+        })
     })
 })
--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -10,7 +10,15 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add(
+    'login',
+    (email = 'goodboy@goodboy.com', password = '123') => {
+        cy.visit('https://wiki-chat.netlify.app/login')
+        cy.get('input[name=email]').type(email)
+        cy.get('input[name=password]').type(password)
+        cy.get('button[type=submit]').click()
+    },
+)
 //
 //
 // -- This is a child command --
