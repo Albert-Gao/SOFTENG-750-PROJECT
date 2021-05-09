51209c0642411cacf0715daa660d1ab04331b0b4
syan386 <55684460+syan386@users.noreply.github.com>
2021-05-09 14:49:39 +0800

    delete unused import
    waitfor gone

--- frontend/src/pages/User/User.test.tsx
+++ frontend/src/pages/User/User.test.tsx
@@ -1,5 +1,5 @@
 import User from './User'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { TestWrapper } from '../../utils/testUtils'
 
