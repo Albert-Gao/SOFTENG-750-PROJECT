c8ca004c7c734e00efa57278d9a2c11259f7daef
Albert Gao <albertgaohy@gmail.com>
2021-05-09 20:40:51 +1200

    fix: added 2 more tests and fixed the original one

--- frontend/src/pages/User/User.test.tsx
+++ frontend/src/pages/User/User.test.tsx
@@ -1,36 +1,84 @@
 import User from './User'
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { TestWrapper } from '../../utils/testUtils'
+import { getOtherUserInfoApi } from '../../api/users.api'
+
+const MOCK_USER_INFO_RESPONSE = {
+    avatar: 'A49bellboy',
+    createdAt: '2021-05-09T08:26:07.783Z',
+    email: 'abc@abc.com',
+    favourites: [],
+    nickName: 'abc',
+    privacy: {
+        shouldShowEmail: true,
+        shouldShowFavouritePage: true,
+        shouldShowSubmittedNews: true,
+    },
+    shouldShowEmail: true,
+    shouldShowFavouritePage: true,
+    shouldShowSubmittedNews: true,
+    updatedAt: '2021-05-09T08:26:07.783Z',
+    _id: '60979c9f9a256508f63a89be',
+}
 
 describe('/User', () => {
-    it('should render the Profile section', () => {
-        const { getByText } = render(
+    it('should fetch the user data', async () => {
+        render(
             <TestWrapper>
                 <User />
             </TestWrapper>,
         )
 
-        expect(getByText('Profile')).toBeInTheDocument()
+        const getOtherUserInfoApiMock = jest.spyOn(getOtherUserInfoApi, 'query')
+
+        await waitFor(() => {
+            expect(getOtherUserInfoApiMock).toHaveBeenCalledTimes(1)
+        })
     })
 
-    it('should render the Favourites section', async () => {
-        const { getByText } = render(
+    it('should render the user info from the response', async () => {
+        const getOtherUserInfoApiMock = jest.spyOn(getOtherUserInfoApi, 'query')
+        getOtherUserInfoApiMock.mockResolvedValue(
+            // @ts-expect-error
+            MOCK_USER_INFO_RESPONSE,
+        )
+
+        const { getAllByText, getByText } = render(
             <TestWrapper>
                 <User />
             </TestWrapper>,
         )
 
-        expect(getByText('Favourites')).toBeInTheDocument()
+        await waitFor(() => {
+            expect(getAllByText(MOCK_USER_INFO_RESPONSE.nickName)).toHaveLength(
+                2,
+            )
+            expect(getByText(MOCK_USER_INFO_RESPONSE.email)).toBeInTheDocument()
+        })
     })
 
-    it('should render the Submitted News section', async () => {
+    it('should hide the user email if the user choose to hide', async () => {
+        const getOtherUserInfoApiMock = jest.spyOn(getOtherUserInfoApi, 'query')
+        MOCK_USER_INFO_RESPONSE.privacy.shouldShowEmail = false
+        getOtherUserInfoApiMock.mockResolvedValue(
+            // @ts-expect-error
+            MOCK_USER_INFO_RESPONSE,
+        )
+
         const { getByText } = render(
             <TestWrapper>
                 <User />
             </TestWrapper>,
         )
 
-        expect(getByText('Submitted News')).toBeInTheDocument()
+        await waitFor(() => {
+            expect(() => getByText(MOCK_USER_INFO_RESPONSE.email)).toThrow(
+                'Unable to find an element',
+            )
+            expect(
+                getByText('This user hides his/her email.'),
+            ).toBeInTheDocument()
+        })
     })
 })
